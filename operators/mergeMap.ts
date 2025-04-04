5b764ed3198f15d2a7355d17417f8cf9faea20ef
KIMSeHyung <tpgud33@gmail.com>
2025-04-05 02:23:09 +0900

    concatMap, exhuastMap, mergeMap

---
+++ operators/mergeMap.ts
@@ -0,0 +1,22 @@
+import { fromEvent, interval, map, mergeMap, take } from "rxjs";
+import { EventEmitter } from "stream";
+
+const emiiter = new EventEmitter();
+
+const click$ = fromEvent(emiiter, 'click');
+
+let clickCount = 0;
+
+click$.pipe(
+    map(() => ++clickCount),
+    mergeMap((count) => 
+        interval(500).pipe(
+            take(3),
+            map((val) => `click ${count}: ${val}`)
+        )
+    )
+).subscribe((val) => console.log(val));
+
+setTimeout(() => emiiter.emit('click'), 0);
+setTimeout(() => emiiter.emit('click'), 200);
+setTimeout(() => emiiter.emit('click'), 400);
